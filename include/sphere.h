a8c2e17d3aff48a4d820545443eb8791d4ed88e1
TomClabault <tomikaddle@orange.fr>
2023-10-29 19:31:54 +0100

    More inlining

--- include/sphere.h
+++ include/sphere.h
@@ -8,7 +8,48 @@ struct Sphere
 {
     Sphere(Point center, float radius, int material_index);
 
-    SYCL_EXTERNAL bool intersect(const Ray& ray, HitInfo& hit_info) const;
+    inline bool Sphere::intersect(const Ray &ray, HitInfo& hit_info) const
+    {
+        Vector L = ray.origin - center;
+
+        //dot(ray._direction, ray._direction) = 1 because direction is normalized
+        constexpr float a = 1.0f;
+        float b = 2.0f * dot(ray.direction, L);
+        float c = dot(L, L) - radius * radius;
+
+        float delta = b * b - 4.0f * a * c;
+        if (delta < 0.0f)
+            return false;
+        else
+        {
+            constexpr float a2 = 2.0f * a;
+
+            if (delta == 0.0f)
+                hit_info.t = -b / a2;
+            else
+            {
+                float sqrt_delta = sycl::sqrt(delta);
+
+                float t1 = (-b - sqrt_delta) / a2;
+                float t2 = (-b + sqrt_delta) / a2;
+
+                if (t1 < t2)
+                {
+                    hit_info.t = t1;
+                    if (hit_info.t < 0.0f)
+                        hit_info.t = t2;
+                }
+            }
+
+            if (hit_info.t < 0.0f)
+                return false;
+
+            hit_info.normal_at_intersection = normalize((ray.origin + ray.direction * hit_info.t) - center);
+            hit_info.material_index = material_index;
+
+            return true;
+        }
+    }
 
     Point center;
     float radius;
--- include/triangle.h
+++ include/triangle.h
@@ -13,7 +13,8 @@ struct Triangle
     Triangle(const Point& a, const Point& b, const Point& c) : m_a(a), m_b(b), m_c(c) {}
 
     SYCL_EXTERNAL Point bbox_centroid() const;
-    //SYCL_EXTERNAL bool intersect(const Ray &ray, HitInfo& hit_info) const;
+
+    //From https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
     inline bool intersect(const Ray& ray, HitInfo& hit_info) const
     {
         const float EPSILON = 0.0000001f;
